718464c4e349b17e6ca78b22cd8592fac2db1434
Febrian Dwi Putra <87922540+fbriandwi@users.noreply.github.com>
2023-08-09 14:55:35 +0700

    Add files via upload

---
+++ 3 - Intro to Conditional Statements/solution.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	if n%2 == 1 {
+		fmt.Println("Weird")
+	} else if n%2 == 0 && n >= 2 && n <= 5 {
+		fmt.Println("Not Weird")
+	} else if n%2 == 0 && n >= 6 && n <= 20 {
+		fmt.Println("Weird")
+	} else if n%2 == 0 && n > 20 {
+		fmt.Println("Not Weird")
+	}
+}
